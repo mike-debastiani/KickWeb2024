a9607a88dbe5f22542c708f7adedb174416c0087
mike-debastiani <mike.debastiani@stud.hslu.ch>
2024-04-09 03:16:36 +0200

    Final

--- Plakat_Versionen/FINAL_09.04_MDB_PlakatProjekt/script.js
+++ Plakat_Versionen/FINAL_09.04_MDB_PlakatProjekt/script.js
@@ -2,6 +2,10 @@ const body = document.body;
 const paths = document.querySelectorAll("path");
 const audio = new Audio("creepyChoir.wav");
 
+let scrollInterval;
+let isUserScrolling = false;
+let isScrollingDown = true; // Neue Variable, um die Scrollrichtung zu verfolgen
+
 paths.forEach((path) => {
   path.addEventListener("mouseover", () => {
     audio.play();
@@ -20,6 +24,8 @@ paths.forEach((path) => {
         otherPath.classList.add("fill-strobe");
       }
     });
+
+    clearInterval(scrollInterval);
   });
   path.addEventListener("mouseout", () => {
     audio.pause();
@@ -39,5 +45,36 @@ paths.forEach((path) => {
         otherPath.classList.remove("fill-strobe");
       }
     });
+
+    scrollInterval = setInterval(autoScroll, 80);
   });
 });
+
+function autoScroll() {
+  var windowHeight = window.innerHeight;
+  var windowScrollTop = window.pageYOffset;
+
+  if (windowScrollTop + windowHeight >= document.body.scrollHeight) {
+    isScrollingDown = false; // Ändere die Scrollrichtung, wenn das Ende erreicht ist
+  } else if (windowScrollTop <= 0) {
+    isScrollingDown = true; // Ändere die Scrollrichtung, wenn ganz oben erreicht ist
+  }
+
+  if (isScrollingDown) {
+    window.scrollBy(0, 1); // Scroll nach unten, wenn isScrollingDown true ist
+  } else {
+    window.scrollBy(0, -1); // Scroll nach oben, wenn isScrollingDown false ist
+  }
+}
+
+document.body.addEventListener("scroll", function () {
+  isUserScrolling = true;
+});
+
+function stopAutoScroll() {
+  if (isUserScrolling) {
+    clearInterval(scrollInterval);
+  }
+}
+
+setInterval(stopAutoScroll, 80);
